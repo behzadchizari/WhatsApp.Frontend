71f7a09ad9a1aec7ab030674e480a777c9ba531a
Behzad Chizari <Behzad Chizari@DESKTOP-44PV0ES>
2023-01-27 17:54:48 +0100

    Add home

---
+++ whatsapp/src/app/home/home.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HomeComponent } from './home.component';
+import { UikitModule } from '../uikit/uikit.module';
+import { ChatModule } from '../chat/chat.module';
+import { ChatListSideModule } from '../chat-list-side/chat-list-side.module';
+
+
+
+@NgModule({
+  declarations: [
+    HomeComponent
+  ],
+  imports: [
+    CommonModule,
+    UikitModule,
+    ChatModule,
+    ChatListSideModule
+  ]
+})
+export class HomeModule { }
