81691c19579b0a5441a0dc7b15e91dec2ccddbff
Behzad Chizari <Behzad Chizari@DESKTOP-44PV0ES>
2023-01-27 23:43:21 +0100

    Add directives module

---
+++ whatsapp/src/app/directives/directives.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ScrollToBottomDirective } from '../directives/scroll-to-bottom.directive';
+
+
+
+@NgModule({
+  declarations: [ScrollToBottomDirective],
+  imports: [
+    CommonModule
+  ],
+  exports: [ScrollToBottomDirective]
+})
+export class DirectivesModule { }
