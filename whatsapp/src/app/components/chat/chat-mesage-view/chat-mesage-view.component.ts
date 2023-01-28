708524e2d00235e75f470657b80d38959f932077
Behzad Chizari <Behzad Chizari@DESKTOP-44PV0ES>
2023-01-28 02:50:25 +0100

    Extract message component

--- whatsapp/src/app/components/chat/chat-mesage-view/chat-mesage-view.component.ts
+++ whatsapp/src/app/components/chat/chat-mesage-view/chat-mesage-view.component.ts
@@ -8,15 +8,15 @@ import { Component } from '@angular/core';
 export class ChatMesageViewComponent {
 
   messages = [
-    { text: 'Hello', out: true },
-    { text: 'Hey there', out: false },
-    { text: 'You good?', out: true },
+    { text: 'Hello', out: true, status: 'read' },
+    { text: 'Hey there', out: false, status: 'read' },
+    { text: 'You good?', out: true, status: 'read' },
     { text: 'Yup', out: false },
-    // { text: 'U?', out: false },
+    { text: 'U?', out: true, status: 'delivered' },
+    { text: 'Let\'s go out To day. I don\'t know if you are busy or not. But it\'s been too boring do far.I need something new and different. Are you upo to eat out tonight or what?', out: true, status: 'sent' },
     // { text: 'Fine', out: true },
     // { text: 'You have time?', out: true },
     // { text: 'sup?', out: false },
-    // { text: 'Let\'s go out To day. I don\'t know if you are busy or not. But it\'s been too boring do far.I need something new and different. Are you upo to eat out tonight or what?', out: true },
     // { text: '?', out: true },
     // { text: 'I\'m in 😅', out: false },
     // { text: 'In publishing and graphic design, Lorem ipsum is a placeholder text commonly used todemonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsummay be used as a placeholder before final copy is available.', out: true },
