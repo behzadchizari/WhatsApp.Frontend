2eb85c7da567668b2dd8d68307559b3019af0378
Behzad Chizari <Behzad Chizari@DESKTOP-44PV0ES>
2023-01-29 18:44:28 +0100

    Add user profile component

---
+++ whatsapp/src/app/components/user-profile-side/user-profile-side.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-user-profile-side',
+  templateUrl: './user-profile-side.component.html',
+  styleUrls: ['./user-profile-side.component.scss']
+})
+export class UserProfileSideComponent {
+
+}
