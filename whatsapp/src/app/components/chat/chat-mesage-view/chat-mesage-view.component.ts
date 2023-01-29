896f3ffc0c7c5ad480849622563eb78f05b25d16
Behzad Chizari <Behzad Chizari@DESKTOP-44PV0ES>
2023-01-29 18:46:25 +0100

    Remove hardcoded messages

--- whatsapp/src/app/components/chat/chat-mesage-view/chat-mesage-view.component.ts
+++ whatsapp/src/app/components/chat/chat-mesage-view/chat-mesage-view.component.ts
@@ -7,27 +7,10 @@ import { Component } from '@angular/core';
 })
 export class ChatMesageViewComponent {
 
-  messages = [
-    // { text: 'Hello', out: true, status: 'read' },
-    // { text: 'Hey there', out: false, status: 'read' },
-    // { text: 'You good?', out: true, status: 'read' },
-    // { text: 'Yup', out: false },
-    // { text: 'U?', out: true, status: 'delivered' },
-    // { text: 'Let\'s go out To day. I don\'t know if you are busy or not. But it\'s been too boring do far.I need something new and different. Are you upo to eat out tonight or what?', out: true, status: 'sent' },
-    // { text: 'Fine', out: true },
-    // { text: 'You have time?', out: true },
-    // { text: 'sup?', out: false },
-    // { text: '?', out: true },
-    // { text: 'I\'m in 😅', out: false },
-    // { text: 'In publishing and graphic design, Lorem ipsum is a placeholder text commonly used todemonstrate the visual form of a document or a typeface without relying on meaningful content. Lorem ipsummay be used as a placeholder before final copy is available.', out: true },
-    // { text: 'So see you next year', out: true },
-    // { text: 'why nex year', out: false },
-    // { text: 'just 4 fun', out: true },
-    // { text: 'LoL!', out: true },
-  ]
+  messages: { text: string, out: boolean }[] = [];
 
   pushMessage() {
-    // this.messages.push({ text: 'ChatMesageViewComponent wroks!', out: true });
+    this.messages.push({ text: 'ChatMesageViewComponent wroks!', out: Math.trunc(Math.random() * 2) === 1 });
   }
 
 }
