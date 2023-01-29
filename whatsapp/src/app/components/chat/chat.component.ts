7a6fc8d3d68ac2a5d59a854782a4348b60a926dd
Behzad Chizari <Behzad Chizari@DESKTOP-44PV0ES>
2023-01-29 03:56:45 +0100

    Parameterize the chat component

--- whatsapp/src/app/components/chat/chat.component.ts
+++ whatsapp/src/app/components/chat/chat.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { ChatItem } from 'src/app/models/chat-item';
 
 @Component({
   selector: 'app-chat',
@@ -6,5 +7,6 @@ import { Component } from '@angular/core';
   styleUrls: ['./chat.component.scss']
 })
 export class ChatComponent {
+  @Input() chatItem!: ChatItem;
 
 }
