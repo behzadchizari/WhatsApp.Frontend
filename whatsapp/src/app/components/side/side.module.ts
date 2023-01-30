9f90cd7a4db87dd726aab5f86efda12dad9b201f
Behzad Chizari <Behzad Chizari@DESKTOP-44PV0ES>
2023-01-30 01:28:50 +0100

    Move user profile to side module

--- whatsapp/src/app/components/side/side.module.ts
+++ whatsapp/src/app/components/side/side.module.ts
@@ -1,15 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { UserProfileSideModule } from './user-profile-side/user-profile-side.module';
 import { SideComponent } from './side.component';
 import { ChatListSideModule } from './chat-list-side/chat-list-side.module';
+import { SideHeaderComponent } from './side-header/side-header.component';
+import { UserProfileSideComponent } from './user-profile-side/user-profile-side.component';
+import { UikitModule } from 'src/app/uikit/uikit.module';
 
 @NgModule({
-  declarations: [SideComponent],
+  declarations: [
+    SideComponent,
+    SideHeaderComponent,
+    UserProfileSideComponent
+  ],
   imports: [
     CommonModule,
+    UikitModule,
     ChatListSideModule,
-    UserProfileSideModule
   ],
   exports: [SideComponent]
 })
