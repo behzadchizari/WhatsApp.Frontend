08f2af47d01f241f8f205bc0a209a5ba250e4897
Behzad Chizari <Behzad Chizari@DESKTOP-44PV0ES>
2023-01-30 01:28:26 +0100

    Add side header component

---
+++ whatsapp/src/app/components/side/side-header/side-header.component.ts
@@ -0,0 +1,15 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-side-header',
+  templateUrl: './side-header.component.html',
+  styleUrls: ['./side-header.component.scss']
+})
+export class SideHeaderComponent {
+  @Input() title!: string;
+  @Output() back = new EventEmitter();
+
+  onBackClick() {
+    this.back.emit();
+  }
+}
