55470ce2cad00322d347c51a141f06be536246fc
Behzad Chizari <Behzad Chizari@DESKTOP-44PV0ES>
2023-02-01 02:06:01 +0100

    Make user profile side indep from side panel

--- whatsapp/src/app/components/side/user-profile-side/user-profile-side.component.ts
+++ whatsapp/src/app/components/side/user-profile-side/user-profile-side.component.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { SideComponentStateService } from 'src/app/services/side-component-state.service';
 
 @Component({
   selector: 'app-user-profile-side',
@@ -7,10 +6,4 @@ import { SideComponentStateService } from 'src/app/services/side-component-state
   styleUrls: ['./user-profile-side.component.scss']
 })
 export class UserProfileSideComponent {
-
-  constructor(private componentStateService: SideComponentStateService) { }
-
-  goBack() {
-    this.componentStateService.clear();
-  }
 }
