30a6652c310bcafa656826d5d89bd987b84839bc
Behzad Chizari <Behzad Chizari@DESKTOP-44PV0ES>
2023-02-02 06:02:51 +0100

    Add change event to editable label

--- whatsapp/src/app/uikit/editable-label/editable-label.component.ts
+++ whatsapp/src/app/uikit/editable-label/editable-label.component.ts
@@ -1,21 +1,34 @@
-import { Component, Input, ViewChild, ElementRef, AfterViewChecked } from '@angular/core';
+import { Component, Input, ViewChild, ElementRef, AfterViewChecked, AfterViewInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-editable-label',
   templateUrl: './editable-label.component.html',
   styleUrls: ['./editable-label.component.scss']
 })
-export class EditableLabelComponent implements AfterViewChecked {
+export class EditableLabelComponent implements AfterViewChecked, AfterViewInit {
   @Input() title!: string;
   @Input() value!: string;
+  @Output() valueChange = new EventEmitter<string>();
 
   @ViewChild('inputRef') inputRef!: ElementRef<HTMLInputElement>;
 
   editMode: boolean = false;
+  newValue!: string;
 
   ngAfterViewChecked() {
     if (this.editMode) {
       this.inputRef.nativeElement.focus();
+    }
+  }
+
+  ngAfterViewInit(): void {
+    this.newValue = this.value
+  }
+
+  submit() {
+    this.toggleEditClick();
+    if (this.newValue !== this.value) {
+      this.valueChange.emit(this.newValue);
     }
   }
 
