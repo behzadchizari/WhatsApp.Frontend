a43d99c27b18e8b536f2f2e1bf3ad5e1c6b692ea
Behzad Chizari <Behzad Chizari@DESKTOP-44PV0ES>
2023-02-03 03:15:45 +0100

    Add pin functionality to chat item

--- whatsapp/src/app/components/side/chat-list-side/chat-item/chat-item.component.ts
+++ whatsapp/src/app/components/side/chat-list-side/chat-item/chat-item.component.ts
@@ -1,5 +1,6 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { ChatItem } from 'src/app/models/chat-item';
+import { ChatService } from 'src/app/services/chat.service';
 
 @Component({
   selector: 'app-chat-item',
@@ -11,8 +12,18 @@ export class ChatItemComponent {
   @Input() chatItem!: ChatItem;
   @Output() itemClick = new EventEmitter<ChatItem>();
 
+  constructor(private chatService: ChatService) { }
+
   onChatItemClick() {
     this.itemClick.emit(this.chatItem);
+  }
+
+  onPinChatItemClick(chatItem: ChatItem) {
+    this.chatService.pinChatItem(chatItem);
+  }
+
+  onUnpinChatItemClick(chatItem: ChatItem) {
+    this.chatService.unPinChatItem(chatItem);
   }
 
   onChatActionClick(event: Event) {
